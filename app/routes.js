53d9df68decef4d46fdde101de66e7e2b240692a
lakshmi Karuppiah <lakshmi.karuppiah@nhs.net>
2020-12-17 17:17:06 +0000

    Account request approve page added

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,16 @@
 // External dependencies
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json()); // to support JSON bodies
+router.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 
 // Add your routes here - above the module.exports line
 
 router.use('/me-journey', require('./views/me-journey/_routes'));
 router.use('/meo-journey', require('./views/meo-journey/_routes'));
+router.use('/version-2', require('./views/version-2/_routes'));
 
 
 module.exports = router;
