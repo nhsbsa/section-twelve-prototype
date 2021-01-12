10a39ee0b974e6d91ba2a3726d779fad2556d7d8
lakshmi Karuppiah <lakshmi.karuppiah@nhs.net>
2021-01-12 17:51:05 +0000

    showing ovm approved details

--- app/routes.js
+++ app/routes.js
@@ -11,6 +11,7 @@ router.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded
 router.use('/me-journey', require('./views/me-journey/_routes'));
 router.use('/meo-journey', require('./views/meo-journey/_routes'));
 router.use('/version-2', require('./views/version-2/_routes'));
+router.use('/version-3', require('./views/version-3/_routes'));
 
 
 module.exports = router;
