71aa46a80dea35b7a94b67c973aa79aea9322f71
StephenHill-NHSBSA <45825845+StephenHill-NHSBSA@users.noreply.github.com>
2022-05-23 08:22:09 +0100

    Updated to v4.8.0 of the prototype kit (with added NHS BSA admin styles)

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,14 @@
 // External dependencies
 const express = require('express');
-
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json()); // to support JSON bodies
+router.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
 
 // Add your routes here - above the module.exports line
+
+router.use('/v1', require('./views/v1/_routes'));
+
 
 module.exports = router;
